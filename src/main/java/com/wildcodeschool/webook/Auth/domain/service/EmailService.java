521362088c33a76921c88ef007aee96ed68e8ccb
Amani_NFZ <118810467+nefzina@users.noreply.github.com>
2024-08-08 09:37:06 +0200

    Update EmailService: added mailjet github secrets

--- src/main/java/com/wildcodeschool/webook/Auth/domain/service/EmailService.java
+++ src/main/java/com/wildcodeschool/webook/Auth/domain/service/EmailService.java
@@ -11,10 +11,10 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService implements IEmailService {
-    @Value("${MJ_APIKEY_PUBLIC}")
+    @Value("${{ secrets.MJ_APIKEY_PUBLIC }}")
     private String apiPublic;
 
-    @Value("${MJ_APIKEY_PRIVATE}")
+    @Value("${{ secrets.MJ_APIKEY_PRIVATE }}")
     private String apiPrivate;
 
     @Override
