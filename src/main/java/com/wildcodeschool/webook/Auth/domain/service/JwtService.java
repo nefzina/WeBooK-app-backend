073d350b0256f62ff62b3cc0bc36d096b28a747a
Amani_NFZ <118810467+nefzina@users.noreply.github.com>
2024-08-09 17:50:44 +0200

    Update JwtService.java

--- src/main/java/com/wildcodeschool/webook/Auth/domain/service/JwtService.java
+++ src/main/java/com/wildcodeschool/webook/Auth/domain/service/JwtService.java
@@ -7,7 +7,6 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
